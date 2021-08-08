323491c75c05cedeb2e5edaf9580794e95007d73
Tom Morris <thomasmorris823@gmail.com>
2021-08-08 20:28:18 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import ImageUploading from "react-images-uploading";
+import React from "react";
 
 function App() {
+
+  const [images, setImages] = React.useState([]);
+  const maxNumber = 2;
+  const onChange = (imageList) => {
+    setImages(imageList);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ImageUploading
+        value={images}
+        onChange={onChange}
+        maxNumber={maxNumber}
+        dataURLKey="data_url"
+      >
+        {({
+          imageList,
+          onImageUpload,
+        }) => (
+          <div className="wrapper">
+            {!imageList[0] && (
+              <button
+              className="uploadButton"
+              onClick={onImageUpload}
+            >
+              Upload Your Art
+            </button>
+            )}
+            {imageList[0] && (
+              <div className="wrapper">
+                <div className="backgroundImg" style={{ backgroundImage: `url(${imageList[0] && imageList[0].data_url})` }}></div>
+                <div className="innerWrapper">
+                  <div className="coverWrapper">
+                    <div className="cover" style={{ backgroundImage: `url(${imageList[0] && imageList[0].data_url})` }}></div>
+                  </div>
+                  <div>
+                    <div className="outer">
+                      <div className="inner" style={{ backgroundImage: `url(${imageList[0] && imageList[0].data_url})` }}>
+                        <div className="innerinner"></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
+            {/* <div className="bambooLink">@bambooleavesuk</div> */}
+          </div>
+        )}
+      </ImageUploading>
+
     </div>
   );
 }
